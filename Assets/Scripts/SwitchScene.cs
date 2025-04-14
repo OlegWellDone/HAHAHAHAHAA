1b15f5d113b5e041377dacb6a4e697d1b22fd410
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2025-04-15 02:07:34 +0300

    ну да я ебанул свет и чо

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+public int sceneNumber;
+public float TimeDelay;
+bool StartTimer = false;
+private float ElapsedTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartTimer){
+            ElapsedTime += Time.deltaTime;
+            if (ElapsedTime > TimeDelay) {
+                SwitchSC(sceneNumber);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartTimer = true;
+    }
+
+    public void SwitchSC(int scene){
+        StartTimer = false;
+        SceneManager.LoadScene(scene);
+    }
+}
